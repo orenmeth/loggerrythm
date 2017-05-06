75f70e45cb45d4111266aaf799c389e5aec66cd6
Oren Meth <orenmeth@gmail.com>
2017-05-06 02:35:41 +0200

    Feat: Add DotNet Framework Client
    - add a dot net framework console application to consume the dot net standard library.

---
+++ Loggerrythm.Console.DotNetFramework/Program.cs
@@ -0,0 +1,20 @@
+﻿using Loggerrythm.Core;
+
+namespace Loggerrythm.Console.DotNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elasticLogger = LoggerFactory.Get(LoggerTypes.ElasticLogger);
+
+            for (var i = 0; i < 100; i++)
+            {
+                elasticLogger.Log(EventLogLevel.Error, $"ElasticLogger: {i}", i);
+            }
+
+            System.Console.WriteLine("Press any key to exit...");
+            System.Console.ReadKey();
+        }
+    }
+}
